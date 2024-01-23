451b10acd346195b953f3e8bcc03e36b200f2275
zahraRostami1999 <zahrarostami137878@gamil.com>
2024-01-23 15:52:56 +0330

    system

--- restaurant/food/models.py
+++ restaurant/food/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Material(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     quantity = models.FloatField()
